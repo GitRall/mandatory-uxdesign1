77775f1ea0f1423dc4e91c347b9d6ea4cb6196d5
GitRall <rasmus_olsson_89@hotmail.com>
2019-01-20 09:17:30 +0100

    minor changes

--- mds.js
+++ mds.js
@@ -1,37 +1,53 @@
-let inputFields = document.querySelectorAll('.mds-text-field__input');
-let switches = document.querySelectorAll('.mds-switch');
 
-let mds = {
-  textField: function(e){
-    let label = e.target.parentNode.childNodes[3];
-    let border = e.target.parentNode.childNodes[5];
-    if(e.type === 'focus' || e.target.value.length > 0){
-      label.style.top = '16px';
-      label.style.fontSize = '1.2rem';
-      label.style.transition = 'all 0.2s';
-      border.style.transform = 'scaleX(1)';
-    }
-    else{
-      label.style.top = '50%';
-      label.style.fontSize = '2rem';
-      label.style.transition = 'all 0.2s';
-      border.style.transform = 'scaleX(0)';
-    }
+window.mds = {
+  textField: function(elements){
+    for(let element of elements){
+      element.addEventListener('focus', function(e){
+        let label = e.target.parentNode.childNodes[3];
+        let border = e.target.parentNode.childNodes[5];
+        if(e.type === 'focus' || e.target.value.length > 0){
+          label.style.top = '16px';
+          label.style.fontSize = '1.2rem';
+          label.style.transition = 'all 0.2s';
+          border.style.transform = 'scaleX(1)';
+        };
+      });
+
+      element.addEventListener('blur', function(e){
+        let label = e.target.parentNode.childNodes[3];
+        let border = e.target.parentNode.childNodes[5];
+        if(e.target.value.length > 0){
+          label.style.top = '16px';
+          label.style.fontSize = '1.2rem';
+          label.style.transition = 'all 0.2s';
+          border.style.transform = 'scaleX(1)';
+        }
+        else{
+          label.style.top = '50%';
+          label.style.fontSize = '2rem';
+          label.style.transition = 'all 0.2s';
+          border.style.transform = 'scaleX(0)';
+        };
+      });
+    };
   },
-  switch: function(e){
-    let track = e.target.childNodes[1];
-    let thumb = track.childNodes[1];
-    if(e.target.dataset.switch === 'disabled'){
-      track.classList.add('mds-switch__track--active');
-      thumb.classList.add('mds-switch__thumb--active');
-      e.target.dataset.switch = 'active';
-    }
-    else{
-      track.classList.remove('mds-switch__track--active');
-      thumb.classList.remove('mds-switch__thumb--active');
-      e.target.dataset.switch = 'disabled';
-    }
-
+  switch: function(elements){
+    for(let element of elements){
+      element.addEventListener('click', function(e){
+        let track = e.target.childNodes[1];
+        let thumb = track.childNodes[1];
+        if(e.target.dataset.switch === 'disabled'){
+          track.classList.add('mds-switch__track--active');
+          thumb.classList.add('mds-switch__thumb--active');
+          e.target.dataset.switch = 'active';
+        }
+        else{
+          track.classList.remove('mds-switch__track--active');
+          thumb.classList.remove('mds-switch__thumb--active');
+          e.target.dataset.switch = 'disabled';
+        };
+      });
+    };
   },
   checkbox: function(){
 
@@ -39,14 +55,4 @@ let mds = {
   radioButton: function(){
 
   },
-}
-
-for(let input of inputFields){
-  input.addEventListener('focus', mds.textField);
-}
-for(let input of inputFields){
-  input.addEventListener('blur', mds.textField);
-}
-for(let x of switches){
-  x.addEventListener('click', mds.switch);
-}
+};
